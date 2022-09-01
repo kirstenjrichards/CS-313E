c874029913875ae06b91246fddad2160733db1bb
swcampbell23 <SWcampbell19@mail.strakejesuit.org>
2022-09-01 12:07:58 -0500

    added spiral.in input file and created spiral.py

---
+++ Spiral.py
@@ -0,0 +1,38 @@
+# File: Sprial.py
+# Description:
+# Student Name:
+# Student UT EID:
+# Partner Name:
+# Partner UT EID:
+# Course Name: CS 313E
+# Unique Number:
+# Date Created:
+# Date Last Modified:
+
+import sys
+
+# Input: n is an odd integer between 1 and 100
+# Output: returns a 2-D list representing a spiral
+#         if n is even add one to n
+def create_spiral( n ):
+    pass
+
+# Input: spiral is a 2-D list and n is an integer
+# Output: returns an integer that is the sum of the 
+#         numbers adjacent to n in the spiral
+#         if n is outside the range return 0
+def sum_adjacent_numbers ( spiral, n ):
+    pass
+
+def main():
+    # read the input file 
+    # create the spiral
+    # add the adjacent numbers 
+    # print the result
+    input_data = sys.stdin.read()
+    data_list = list(input_data.split())
+    create_spiral( data_list[0])
+
+if __name__ == "__main__":
+    main()
+
