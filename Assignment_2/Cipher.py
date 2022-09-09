c03de0afa85c91fa67f16d7a929e3424e39d89bf
swcampbell23 <SWcampbell19@mail.strakejesuit.org>
2022-09-09 11:16:34 -0500

    Added function encrypt and input reading from the main function

--- Assignment_2/Cipher.py
+++ Assignment_2/Cipher.py
@@ -1,9 +1,68 @@
+from cgitb import small
 import math
+import sys
 # Input: strng is a string of 100 or less of upper case, lower case, 
 #        and digits
 # Output: function returns an encrypted string 
 def encrypt ( strng ):
-  pass
+
+  # The below block of code is designed to create variables 
+  # in preparation of creating a table
+  input_len = len(strng)
+  smallest_square = 1
+  while input_len > (smallest_square ** 2):
+    smallest_square += 1
+
+  # The below block of code creates an empty 2d list with
+  # the given variables above
+  encrypt_list = []
+  for row in range(smallest_square):
+      encrypt_row = []
+      for col in range(smallest_square):
+        encrypt_row.append(0)
+      encrypt_list.append(encrypt_row)
+
+  # The below block of code updates the empty list with 
+  # the string input value and fills empty spaces with
+  # an asterisk
+  strng_value = 0
+  for row in range(smallest_square):
+    for col in range(smallest_square):
+      if strng_value >= input_len:
+        encrypt_list[row][col] = '*'
+      else:
+        encrypt_list[row][col] = strng[strng_value]
+        strng_value += 1
+
+  # The below block of code is designed to create an empty 
+  # list for the use of rotation
+  rotate_list = []
+  for row in range(smallest_square):
+      encrypt_row = []
+      for col in range(smallest_square):
+        encrypt_row.append(0)
+      rotate_list.append(encrypt_row)
+
+  # The below block of code adds the contents of the original
+  # 2d list into the new list rotated 90 degrees
+  encrypt_row = smallest_square - 1
+  for rotate_col in range(smallest_square):
+    encrypt_word = encrypt_list[encrypt_row]
+    for rotate_row in range(smallest_square):
+      rotate_list[rotate_row][rotate_col] = encrypt_word[rotate_row]
+    encrypt_row -= 1
+
+  # The below block of code converts the rotated 2d list 
+  # into a string omitting the asterisk
+  encrypt_word = ''
+  for row in range(smallest_square):
+    for col in range(smallest_square):
+      if rotate_list[row][col] == '*':
+        pass
+      else:
+        encrypt_word += rotate_list[row][col]
+
+  return encrypt_word
 
 # Input: strng is a string of 100 or less of upper case, lower case, 
 #        and digits
@@ -13,14 +72,18 @@ def decrypt ( strng ):
 
 def main():
   # read the strings P and Q from standard input
+  input_data = sys.stdin.read()
+  global data_list
+  data_list = list(input_data.split())
 
   # encrypt the string P
+  encrypt_p = encrypt(data_list[0])
 
   # decrypt the string Q
 
   # print the encrypted string of P
+  print(encrypt_p)
   # and the decrypted string of Q
-  pass
 
 if __name__ == "__main__":
   main()
