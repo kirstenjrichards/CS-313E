2c411dc3db655930c37c00404952792289cba61c
swcampbell23 <SWcampbell19@mail.strakejesuit.org>
2022-10-16 12:59:40 -0500

    tried

--- Assignment_7/ExpressionTree.py
+++ Assignment_7/ExpressionTree.py
@@ -38,6 +38,9 @@ class Stack (object):
     def is_empty(self):
         return len(self.stack) == 0
 
+    def __str__(self):
+        return str(self.stack)
+
 class Node (object):
     def __init__ (self, data = None, lChild = None, rChild = None):
         self.data = data
@@ -47,24 +50,50 @@ class Node (object):
 class Tree (object):
     def __init__ (self):
         self.root = None
+        self.tree_stack = Stack()
     
     # this function takes in the input string expr and 
     # creates the expression tree
     def create_tree (self, expr):
-    
+        current_node = Node()
+
+        for x in range(len(expr)):
+            current_tok = expr[x]
+
+            if current_tok == '(':
+                current_node.lChild = Node()
+                self.tree_stack.push(current_node)
+                current_node = current_node.lChild
+            
+            elif current_tok in operators:
+                current_node.data = current_tok
+                self.tree_stack.push(current_node)
+                current_node.rChild = Node()
+                current_node = current_node.rChild
+            
+            elif current_tok == ')':
+                self.tree_stack.pop()
+
+            else:
+                current_node.data = current_tok
+                self.tree_stack.pop()
+        
     # this function should evaluate the tree's expression
     # returns the value of the expression after being calculated
     def evaluate (self, aNode):
+        pass
     
     # this function should generate the preorder notation of 
     # the tree's expression
     # returns a string of the expression written in preorder notation
     def pre_order (self, aNode):
+        pass
 
     # this function should generate the postorder notation of 
     # the tree's expression
     # returns a string of the expression written in postorder notation
     def post_order (self, aNode):
+        pass
 
 # you should NOT need to touch main, everything should be handled for you
 def main():
@@ -74,15 +103,16 @@ def main():
  
     tree = Tree()
     tree.create_tree(expr)
+    print(tree.tree_stack)
     
     # evaluate the expression and print the result
-    print(expr, "=", str(tree.evaluate(tree.root)))
+    # print(expr, "=", str(tree.evaluate(tree.root)))
 
     # get the prefix version of the expression and print
-    print("Prefix Expression:", tree.pre_order(tree.root).strip())
+    # print("Prefix Expression:", tree.pre_order(tree.root).strip())
 
     # get the postfix version of the expression and print
-    print("Postfix Expression:", tree.post_order(tree.root).strip())
+    # print("Postfix Expression:", tree.post_order(tree.root).strip())
 
 if __name__ == "__main__":
     main()
